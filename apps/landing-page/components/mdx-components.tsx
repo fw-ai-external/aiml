481bc5ce090036e55050ae762ab6cdb6ff48ba7f
Matt Apperson <me@mattapperson.com>
2025-04-11 18:18:55 -0400

    Docs initial page

--- apps/landing-page/components/mdx-components.tsx
+++ apps/landing-page/components/mdx-components.tsx
@@ -1,9 +1,10 @@
 import defaultMdxComponents from "fumadocs-ui/mdx";
 import type { MDXComponents } from "mdx/types";
-
+import { Mermaid } from "./mdx/mermaid";
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
+    Mermaid,
     ...components,
   };
 }
