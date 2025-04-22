b7bbc278b534d65b7375f57ef5ce7d918a6b194d
Matt Apperson <me@mattapperson.com>
2025-04-22 16:23:51 -0400

    cruft

--- apps/server/src/lib/errors.ts
+++ apps/server/src/lib/errors.ts
@@ -1,45 +1,46 @@
 // eslint-disable-next-line eslint-comments/disable-enable-pair
 /* eslint-disable @typescript-eslint/restrict-template-expressions */
-import { Context } from 'hono';
-import { HTTPException } from 'hono/http-exception';
-import { ZodError } from 'zod';
-import { generateErrorMessage } from 'zod-error';
+import type { Context } from "hono";
+import { HTTPException } from "hono/http-exception";
+import type { ZodError } from "zod";
+import { generateErrorMessage } from "zod-error";
 
-import { z } from '@hono/zod-openapi';
-import { StatusCode } from 'hono/utils/http-status';
+import { z } from "@hono/zod-openapi";
+import type { ContentfulStatusCode } from "hono/utils/http-status";
 
 const ErrorCode = z.enum([
-  'BAD_REQUEST',
-  'FORBIDDEN',
-  'INTERNAL_SERVER_ERROR',
-  'USAGE_EXCEEDED',
-  'INVALID_KEY_TYPE',
-  'DISABLED',
-  'NOT_FOUND',
-  'NOT_UNIQUE',
-  'RATE_LIMITED',
-  'UNAUTHORIZED',
-  'PRECONDITION_FAILED',
-  'INSUFFICIENT_PERMISSIONS',
+  "BAD_REQUEST",
+  "FORBIDDEN",
+  "INTERNAL_SERVER_ERROR",
+  "USAGE_EXCEEDED",
+  "INVALID_KEY_TYPE",
+  "DISABLED",
+  "NOT_FOUND",
+  "NOT_UNIQUE",
+  "RATE_LIMITED",
+  "UNAUTHORIZED",
+  "PRECONDITION_FAILED",
+  "INSUFFICIENT_PERMISSIONS",
 ]);
 
 export function errorSchemaFactory(code: z.ZodEnum<any>) {
   return z.object({
     error: z.object({
       code: code.openapi({
-        description: 'A machine readable error code.',
+        description: "A machine readable error code.",
         example: code._def.values.at(0),
       }),
       docs: z.string().openapi({
-        description: 'A link to our documentation with more details about this error code',
+        description:
+          "A link to our documentation with more details about this error code",
         example: `https://fireworks.ai/docs/api-reference/errors/code/${code._def.values.at(0)}`,
       }),
       message: z.string().openapi({
-        description: 'A human readable explanation of what went wrong',
+        description: "A human readable explanation of what went wrong",
       }),
       requestId: z.string().openapi({
-        description: 'Please always include the requestId in your error report',
-        example: 'req_1234',
+        description: "Please always include the requestId in your error report",
+        example: "req_1234",
       }),
     }),
   });
@@ -48,19 +49,21 @@ export function errorSchemaFactory(code: z.ZodEnum<any>) {
 export const ErrorSchema = z.object({
   error: z.object({
     code: ErrorCode.openapi({
-      description: 'A machine readable error code.',
-      example: 'INTERNAL_SERVER_ERROR',
+      description: "A machine readable error code.",
+      example: "INTERNAL_SERVER_ERROR",
     }),
     docs: z.string().openapi({
-      description: 'A link to our documentation with more details about this error code',
-      example: 'https://fireworks.ai/docs/api-reference/errors/code/BAD_REQUEST',
+      description:
+        "A link to our documentation with more details about this error code",
+      example:
+        "https://fireworks.ai/docs/api-reference/errors/code/BAD_REQUEST",
     }),
     message: z.string().openapi({
-      description: 'A human readable explanation of what went wrong',
+      description: "A human readable explanation of what went wrong",
     }),
     requestId: z.string().openapi({
-      description: 'Please always include the requestId in your error report',
-      example: 'req_1234',
+      description: "Please always include the requestId in your error report",
+      example: "req_1234",
     }),
   }),
 });
@@ -69,22 +72,22 @@ export type ErrorResponse = z.infer<typeof ErrorSchema>;
 
 function codeToStatus(code: z.infer<typeof ErrorCode>): number {
   switch (code) {
-    case 'BAD_REQUEST':
+    case "BAD_REQUEST":
       return 400;
-    case 'PRECONDITION_FAILED':
+    case "PRECONDITION_FAILED":
       return 412;
-    case 'NOT_FOUND':
+    case "NOT_FOUND":
       return 404;
-    case 'FORBIDDEN':
-    case 'DISABLED':
-    case 'UNAUTHORIZED':
-    case 'INSUFFICIENT_PERMISSIONS':
+    case "FORBIDDEN":
+    case "DISABLED":
+    case "UNAUTHORIZED":
+    case "INSUFFICIENT_PERMISSIONS":
       return 403;
-    case 'INVALID_KEY_TYPE':
-    case 'USAGE_EXCEEDED':
-    case 'NOT_UNIQUE':
-    case 'RATE_LIMITED':
-    case 'INTERNAL_SERVER_ERROR':
+    case "INVALID_KEY_TYPE":
+    case "USAGE_EXCEEDED":
+    case "NOT_UNIQUE":
+    case "RATE_LIMITED":
+    case "INTERNAL_SERVER_ERROR":
       return 500;
   }
 }
@@ -99,7 +102,7 @@ export class FireworksApiError extends HTTPException {
     code: z.infer<typeof ErrorCode>;
     message: string;
   }) {
-    super(codeToStatus(code) as StatusCode, { message });
+    super(codeToStatus(code) as ContentfulStatusCode, { message });
     this.code = code;
   }
 }
@@ -114,38 +117,39 @@ export function handleZodError(
         success: false;
         error: ZodError;
       },
-  c: Context,
+  c: Context
 ) {
   if (!result.success) {
     return c.json<z.infer<typeof ErrorSchema>>(
       {
         error: {
-          code: 'BAD_REQUEST',
-          docs: 'https://fireworks.ai/docs/api-reference/errors/code/BAD_REQUEST',
+          code: "BAD_REQUEST",
+          docs: "https://fireworks.ai/docs/api-reference/errors/code/BAD_REQUEST",
           message: generateErrorMessage(result.error.issues, {
             maxErrors: 1,
             delimiter: {
-              component: ': ',
+              component: ": ",
             },
             path: {
               enabled: true,
-              type: 'objectNotation',
-              label: '',
+              type: "objectNotation",
+              label: "",
             },
             code: {
               enabled: true,
-              label: '',
+              label: "",
             },
             message: {
               enabled: true,
-              label: '',
+              label: "",
             },
-            suffix: ', See "https://fireworks.ai/docs/api-reference" for more details',
+            suffix:
+              ', See "https://fireworks.ai/docs/api-reference" for more details',
           }),
-          requestId: c.get('requestId'),
+          requestId: c.get("requestId"),
         },
       },
-      { status: 400 },
+      { status: 400 }
     );
   }
 }
@@ -158,10 +162,10 @@ export function handleError(err: Error, c: Context): Response {
           code: err.code,
           docs: `https://fireworks.ai/docs/api-reference/errors/code/${err.code}`,
           message: err.message,
-          requestId: c.get('requestId'),
+          requestId: c.get("requestId"),
         },
       },
-      { status: err.status },
+      { status: err.status }
     );
   }
   console.error(err);
@@ -169,26 +173,30 @@ export function handleError(err: Error, c: Context): Response {
   return c.json<z.infer<typeof ErrorSchema>>(
     {
       error: {
-        code: 'INTERNAL_SERVER_ERROR',
-        docs: 'https://fireworks.ai/docs/api-reference/errors/code/INTERNAL_SERVER_ERROR',
-        message: 'something unexpected happened',
-        requestId: c.get('requestId'),
+        code: "INTERNAL_SERVER_ERROR",
+        docs: "https://fireworks.ai/docs/api-reference/errors/code/INTERNAL_SERVER_ERROR",
+        message: "something unexpected happened",
+        requestId: c.get("requestId"),
       },
     },
-    { status: 500 },
+    { status: 500 }
   );
 }
 
-export function errorResponse(c: Context, code: z.infer<typeof ErrorCode>, message: string) {
+export function errorResponse(
+  c: Context,
+  code: z.infer<typeof ErrorCode>,
+  message: string
+) {
   return c.json<z.infer<typeof ErrorSchema>>(
     {
       error: {
         code,
         docs: `https://fireworks.ai/docs/api-reference/errors/code/${code}`,
         message,
-        requestId: c.get('requestId'),
+        requestId: c.get("requestId"),
       },
     },
-    { status: codeToStatus(code) },
+    { status: codeToStatus(code) as ContentfulStatusCode }
   );
 }
