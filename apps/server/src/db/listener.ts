8c326b49a1e0b8b5fcb33bd13f5d28a8d378ac43
Matt Apperson <me@mattapperson.com>
2025-05-21 16:49:39 -0400

    remove listeners for now...

--- apps/server/src/db/listener.ts
+++ apps/server/src/db/listener.ts
@@ -1,49 +1,49 @@
 import { z } from "zod";
-import { postgresClient } from "./conn.ts";
-import { NewAgentSpecificationVersion, camelCaseKeys } from "./schema.ts";
+// import { postgresClient } from "./conn.ts";
+// import { NewAgentSpecificationVersion, camelCaseKeys } from "./schema.ts";
 
-export const ChannelMap = {
-  new_agent_version: z.preprocess(camelCaseKeys, NewAgentSpecificationVersion),
-};
+// export const ChannelMap = {
+//   new_agent_version: z.preprocess(camelCaseKeys, NewAgentSpecificationVersion),
+// };
 
-export type ChannelMap = typeof ChannelMap;
+// export type ChannelMap = typeof ChannelMap;
 
-export const setupListeners = () =>
-  Promise.all([
-    new Promise<void>((resolve) => {
-      getDBListener({
-        channel: "new_agent_version",
-        onNotify: (payload) => {
-          // Agent version received
-        },
-        onListen: async () => {
-          // Now listening for new agent versions
-          resolve();
-        },
-      });
-    }),
-  ]);
+// export const setupListeners = () =>
+//   Promise.all([
+//     new Promise<void>((resolve) => {
+//       getDBListener({
+//         channel: "new_agent_version",
+//         onNotify: (payload) => {
+//           // Agent version received
+//         },
+//         onListen: async () => {
+//           // Now listening for new agent versions
+//           resolve();
+//         },
+//       });
+//     }),
+//   ]);
 
-type ListenerConfig<Channel extends keyof ChannelMap> = {
-  channel: Channel;
-  onNotify: (payload: z.infer<ChannelMap[Channel]>) => void;
-  onListen: () => void;
-};
+// type ListenerConfig<Channel extends keyof ChannelMap> = {
+//   channel: Channel;
+//   onNotify: (payload: z.infer<ChannelMap[Channel]>) => void;
+//   onListen: () => void;
+// };
 
-export const getDBListener = <Channel extends keyof ChannelMap>(
-  props: ListenerConfig<Channel>
-) => {
-  return postgresClient.listen(
-    props.channel,
-    (payloadString: string) => {
-      const payload = ChannelMap[props.channel].parse(
-        JSON.parse(payloadString)
-      );
-      props.onNotify(payload);
-    },
-    props.onListen
-  );
-};
+// export const getDBListener = <Channel extends keyof ChannelMap>(
+//   props: ListenerConfig<Channel>
+// ) => {
+//   return postgresClient.listen(
+//     props.channel,
+//     (payloadString: string) => {
+//       const payload = ChannelMap[props.channel].parse(
+//         JSON.parse(payloadString)
+//       );
+//       props.onNotify(payload);
+//     },
+//     props.onListen
+//   );
+// };
 
 import { fireworks } from "@ai-sdk/fireworks";
 import {
